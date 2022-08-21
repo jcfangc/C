7b3d5b1f559d417a21fca3e9ade5758bb18cb0dd
jcfangc <111571686+jcfangc@users.noreply.github.com>
2022-08-21 17:38:30 +0800

    Add files via upload
    原函数中有些错误，我们更正这些错误

---
+++ cha9_pra1.c
@@ -0,0 +1,15 @@
+// 改错题
+// double triangle_area(double base, height)
+// double product;
+// {
+//   product = base * height;
+//   return product / 2;
+// }
+
+// 参考答案
+// double triangle_area(double base, double height)
+// {
+//   double product;
+//   product = base * height;
+//   return (product / 2);
+// }
