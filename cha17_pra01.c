41e0b7bd888a938ff6cd566774440a5261b85951
jcfangc <111571686+jcfangc@users.noreply.github.com>
2022-11-19 23:50:27 +0000

    Update cha17_pra01.c
    修改了 main 函数，增加了 19 行的 free 函数。

--- cha17_pra01.c
+++ cha17_pra01.c
@@ -1,4 +1,4 @@
-// ÿ�ε���ʱ����麯�� malloc ���������κ��ڴ���亯�����ķ���ֵ�Ǽ��ܷ��˵����顣���дһ����Ϊ my_malloc �ĺ�����Ϊ malloc ������ ����װ�����������ú��� my_malloc ��Ҫ����� n ���ֽ�ʱ��������� malloc �������ж� malloc ����ȷʵû�з��ؿ�ָ�룬Ȼ�󷵻����� malloc ��ָ�롣��� malloc ���ؿ�ָ�룬��ô���� my_malloc ��ʾ������Ϣ����ֹ����
+// 每次调用时都检查函数 malloc （或其他任何内存分配函数）的返回值是件很烦人的事情。请编写一个名为 my_malloc 的函数作为 malloc 函数的 “包装器”。当调用函数 my_malloc 并要求分配 n 个字节时，它会调用 malloc 函数，判断 malloc 函数确实没有返回空指针，然后返回来自 malloc 的指针。如果 malloc 返回空指针，那么函数 my_malloc 显示出错信息并终止程序。
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,6 +16,7 @@ int main(void)
     str = my_malloc(N * sizeof(char));
     strncpy(str, "My_malloc works well.", N);
     printf("\n\t %s", str);
+    free(str);
 
     getchar();
     return 0;
