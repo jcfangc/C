9e8e4711023f400c700671bde87e2241049cd651
jcfangc <111571686+jcfangc@users.noreply.github.com>
2022-12-12 20:43:02 +0000

    枚举的标记和名字

---
+++ cha16_pra15.c
@@ -0,0 +1,6 @@
+// (a) 为枚举声明标记，此枚举的值表示一周中的 7 天。
+enum week_day { MON, TUE, WEN, THU, FRI, SAT, SUN };
+// (b) 用 typedef 定义(a)中枚举的名字。
+typedef enum { MON, TUE, WEN, THU, FRI, SAT, SUN } week_day;
+
+// 书作者答案链接：http://knking.com/books/c2/answers/c16.html
